ddc6a854439182d1cec8905b9f7ca536e6c7c4f9
l2ujiuggl <l2ujiuggl@gmail.com>
2019-04-07 10:15:26 +0800

    【new】 新增羅馬數字轉換中文數字

---
+++ 20190407_Rome/app/src/main/java/com/view/rome/MainActivity.java
@@ -0,0 +1,20 @@
+package com.view.rome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("hemly","data= "+RomanNumeral.intValue("IX"));
+        Log.e("hemly","data= "+RomanNumeral.intValue("CCCXCIV"));
+        Log.e("hemly","data= "+RomanNumeral.intValue("MCM"));
+    }
+
+
+}
